33f7524d364c3048eb355c0af306a77cf5219ddc
andrey <6097033725>
2020-04-08 17:57:34 +0300

    first commit

---
+++ src/DatePickerHelper.php
@@ -0,0 +1,43 @@
+<?php
+namespace andrewdanilov\helpers;
+
+use Yii;
+
+class DatePickerHelper
+{
+	public static function defaultOptions()
+	{
+		return [
+			'language' => Yii::$app->language,
+			'template' => '{input}{addon}',
+			'clientOptions' => [
+				'autoclose' => true,
+				'format' => 'dd.mm.yyyy',
+				'clearBtn' => true,
+				'todayBtn' => 'linked',
+			],
+			'clientEvents' => [
+				'clearDate' => 'function (e) {$(e.target).find("input").change();}',
+			],
+		];
+	}
+
+	public static function defaultFilterOptions($searchModel, $attribute)
+	{
+		return [
+			'model' => $searchModel,
+			'attribute' => $attribute,
+			'language' => Yii::$app->language,
+			'template' => '{input}{addon}',
+			'clientOptions' => [
+				'autoclose' => true,
+				'format' => 'dd.mm.yyyy',
+				'clearBtn' => true,
+				'todayBtn' => 'linked',
+			],
+			'clientEvents' => [
+				'clearDate' => 'function (e) {$(e.target).find("input").change();}',
+			],
+		];
+	}
+}
