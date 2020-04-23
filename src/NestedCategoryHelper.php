efb7411a81790c009aab502698f6669c8e145c20
andrey <6097033725>
2020-04-24 02:22:45 +0300

    Misc

--- src/NestedCategoryHelper.php
+++ src/NestedCategoryHelper.php
@@ -2,6 +2,7 @@
 namespace andrewdanilov\helpers;
 
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 /**
  * NestedCategoryHelper class
@@ -12,15 +13,16 @@ class NestedCategoryHelper
 
 	/**
 	 * @param ActiveRecord[] $categories
+	 * @param string $parent_key
 	 * @return null
 	 */
-	public static function getGroupedCategories($categories)
+	public static function getGroupedCategories($categories, $parent_key='parent_id')
 	{
 		if (static::$_groupedCategories === null) {
 			static::$_groupedCategories = [];
 			foreach ($categories as $category) {
-				if (property_exists($category, 'parent_id')) {
-					static::$_groupedCategories[$category->parent_id][$category->getPrimaryKey()] = $category;
+				if (property_exists($category, $parent_key)) {
+					static::$_groupedCategories[$category->getAttribute($parent_key)][$category->getPrimaryKey()] = $category;
 				}
 			}
 		}
@@ -33,17 +35,18 @@ class NestedCategoryHelper
 	 *
 	 * @param ActiveRecord[] $categories
 	 * @param int $parent_id
+	 * @param string $parent_key
 	 * @param int $level
 	 * @return array
 	 */
-	public static function getDropdownTree($categories, $parent_id=0, $level=0) {
+	public static function getDropdownTree($categories, $parent_id=0, $parent_key='parent_id', $level=0) {
 		$tree = [];
-		$grouped_categories = static::getGroupedCategories($categories);
+		$grouped_categories = static::getGroupedCategories($categories, $parent_key);
 		if (isset($grouped_categories[$parent_id])) {
 			foreach ($grouped_categories[$parent_id] as $id => $category) {
 				$tree[$id] = str_repeat('│ ', $level) . '├ ' . $category->name;
 				if (isset($grouped_categories[$id])) {
-					$tree += static::getDropdownTree($categories, $id, $level + 1);
+					$tree += static::getDropdownTree($categories, $id, $parent_key, $level + 1);
 				}
 			}
 		}
@@ -55,11 +58,41 @@ class NestedCategoryHelper
 	 *
 	 * @param ActiveRecord[] $categories
 	 * @param int $parent_id
+	 * @param string $parent_key
 	 * @return array
 	 */
-	public static function getChildrenIds($categories, $parent_id=0)
+	public static function getChildrenIds($categories, $parent_id=0, $parent_key='parent_id')
 	{
-		$tree = static::getDropdownTree($categories, $parent_id);
+		$tree = static::getDropdownTree($categories, $parent_id, $parent_key);
 		return array_keys($tree);
+	}
+
+	/**
+	 * Returns path to category as delimited string
+	 *
+	 * @param ActiveRecord[] $categories
+	 * @param int $category_id
+	 * @param string $name_attribute
+	 * @param string $parent_key
+	 * @param string $delimiter
+	 * @return string
+	 */
+	public static function getCategoryPath($categories, $category_id, $name_attribute='name', $parent_key='parent_id', $delimiter='/')
+	{
+		$categories_indexed = ArrayHelper::map($categories, 'id', $name_attribute);
+		$path = [];
+		if (isset($categories_indexed[$category_id])) {
+			$parent_id = $category_id;
+			do {
+				foreach ($categories_indexed as $category) {
+					if ($category['id'] == $parent_id) {
+						$path[] = $category[$name_attribute];
+						$parent_id = $category[$parent_key];
+					}
+				}
+			} while ($parent_id !== 0);
+			rsort($path);
+		}
+		return implode($delimiter, $path);
 	}
 }
